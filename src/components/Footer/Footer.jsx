df52b1de3c195e02b3aa7b02446e8e1798ab7bfa
Prajwal K Kammardi <pkamma2@uic.edu>
2021-01-11 14:27:26 -0600

    Edited typos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by Jacobo Martínez
+          © Prajwal Kammardi{new Date().getFullYear()} - Original Template credits - Jacobo Martínez
         </p>
 
         {isEnabled && <GithubButtons />}
