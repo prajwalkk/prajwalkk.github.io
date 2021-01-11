ade6812f6831de3660e0b4c3314e02dc90be4182
Prajwal K Kammardi <pkamma2@uic.edu>
2021-01-11 14:40:28 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6309052fd4f16d7228e9.js"
+    "url": "webpack-runtime-9d01ca8c80db3e3081f8.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "de085498bfe4d3a7f1d883b5faa64154"
+    "revision": "b911957db10172bb90ba21076911a9e9"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "638827a0e958535bf555ce6c5cb4919e"
+    "revision": "b2b4f2bcb7670b9ced587b2e0327f72d"
   },
   {
     "url": "polyfill-e4957c11759bdfc149d9.js"
